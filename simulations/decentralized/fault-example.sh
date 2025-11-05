af057eae8541bd5dd5d7106747789f00ff3bffa7
gaochuanchao <gaoc0008@e.ntu.edu.sg>
2025-11-05 21:20:20 +0800

    test fault injection

---
+++ simulations/decentralized/fault-example.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+cd ${MEC_WORKSPACE}/mecRT/simulations/decentralized
+
+echo "============================================="
+echo "Running configuration LinkFailure"
+echo "============================================="
+
+${OMNETPP_ROOT}/bin/opp_run \
+  -r 0 \
+  -m -u Cmdenv \
+  -c LinkFailure \
+  -n "../../src:..:../../../simu5g/emulation:../../../simu5g/simulations:../../../simu5g/src:../../../inet4.5/examples:../../../inet4.5/showcases:../../../inet4.5/src:../../../inet4.5/tests/validation:../../../inet4.5/tests/networks:../../../inet4.5/tutorials" \
+  --image-path "../../images:../../../inet4.5/images:../../../simu5g/images" \
+  -l "../../src/mecrt" \
+  -l "../../../simu5g/src/simu5g" \
+  -l "../../../inet4.5/src/INET" \
+  omnetpp.ini \
+  --sim-time-limit=900s
+
+echo "Finished run 0 for LinkFailure"
+
+
+
+# for i in {0..2}; do  # 24 configurations
+#   echo "============================================="
+#   echo "Running configuration $i for NodeFailure"
+#   echo "============================================="
+
+#   ${OMNETPP_ROOT}/bin/opp_run \
+#     -r $i \
+#     -m -u Cmdenv \
+#     -c NodeFailure \
+#     -n "../../src:..:../../../simu5g/emulation:../../../simu5g/simulations:../../../simu5g/src:../../../inet4.5/examples:../../../inet4.5/showcases:../../../inet4.5/src:../../../inet4.5/tests/validation:../../../inet4.5/tests/networks:../../../inet4.5/tutorials" \
+#     --image-path "../../images:../../../inet4.5/images:../../../simu5g/images" \
+#     -l "../../src/mecrt" \
+#     -l "../../../simu5g/src/simu5g" \
+#     -l "../../../inet4.5/src/INET" \
+#     omnetpp.ini \
+#     --sim-time-limit=900s
+
+#   echo "Finished run $i for NodeFailure"
+# done
+
